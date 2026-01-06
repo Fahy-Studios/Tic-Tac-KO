90a16d025e28e2061f03542c64aaa1a146871f08
info <info@fahy.gg>
2026-01-06 15:39:03 +0000

    Initialize Capacitor and add Android build workflow

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fahystudios.tictacko',
+  appName: 'Tic-Tac-KO',
+  webDir: 'dist'
+};
+
+export default config;
